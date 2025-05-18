a765b6b6631b42592d41e52becea9f4dfeba6113
Layla Paula <layhpaula@gmail.com>
2025-05-18 17:05:13 -0300

    refactor: CORREÇÃO arquivo teste com erro proposital

--- test_example.py
+++ test_example.py
@@ -1,6 +1,13 @@
+# def soma(a, b):
+#     return a + b
+#
+# def test_soma():
+#     assert soma(2, 2) == 5
+#  # Erro proposital 2 + 2 = 4
+
 def soma(a, b):
     return a + b
 
+
 def test_soma():
-    assert soma(2, 2) == 5
- # Erro proposital 2 + 2 = 4
+    assert soma(2, 2) == 4
